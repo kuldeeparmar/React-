154784d6e0b44d1ec90ac8b64417598a15c3176e
Kuldeep Parmar <kuldeepparmar246@gmail.com>
2024-08-18 17:54:08 +0530

    Explore lazy and Suspense

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useCallback, useEffect ,useRef , useMemo} from "react";
+import React, { useState, lazy,useCallback,Suspense ,useEffect ,useRef , useMemo} from "react";
 import Button from "./Button.jsx";
-import Text from "./Text.jsx";
+//import Text from "./Text.jsx";
 import Timer from "./Timer.jsx";
 import ButtonWithTooltip from './ButtonWithTooltip.jsx'
 import Input from './Input.jsx'
@@ -8,28 +8,25 @@ import SecondParent from './SecondParent.jsx'
 
 import PrintTable from './PrintTable.jsx'
 
+function delayForDemo(promise) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 2000);
+  }).then(() => promise);
+}
 
+const Text = lazy(() => delayForDemo(import('./Text.jsx')));
 const App = () => {
 
-  const [counter1,setCounter1] = useState(0);
-  const [counter2,setCounter2] = useState(0);
-  const tmp = 10;
-  const obj = {
-    name : 'Kuldeep'
-  }
-  const arr =useMemo(() => [1,2] ,[]);
-
+  const [show,setShow] = useState(false);
   return (
     <>
-    Counter 1 : {counter1}
-    <button onClick={() => setCounter1((prev) => prev+1)}>counter1++</button>  
-    <br/>
-    Counter 2 : {counter2}
-    <button onClick={() => setCounter2((prev) => prev+1)}>counter2++</button>
-    <br/>
 
-    <PrintTable num={counter1} tmp={tmp} arr={arr}/>
-    
+    <button onClick={() => setShow(!show)}>Show</button>
+
+    {show && <Suspense fallback={<div>Loading...</div>}>
+      <Text children="Hello World"/>
+    </Suspense>
+    }
     </>
 
     
@@ -38,3 +35,5 @@ const App = () => {
 };
 
 export default App;
+
+
