e5ceb37ec339170c668c2af7eca86a6469338486
Kuldeep Parmar <kuldeepparmar246@gmail.com>
2024-08-19 22:52:39 +0530

    fetch Api debounce and opitimization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React,{useEffect,useState} from "react";
+import React,{useEffect,useState,useMemo, useCallback} from "react";
 import { Route,Routes,Link,useParams } from "react-router-dom"; 
 import Comp1 from './Comp1.jsx'
 import InputProps from "./InputProps.jsx";
 import useOnline from './useOnline.js'
+import debounce from 'debounce';
 
 const App = () => {
 
- const [photos,setPhotos] = useState();
+  const [data,setData] = useState();
+  const [text,setText] = useState('')
+
+  const fetchData = useMemo(() => debounce(async (value) => {
+
+    const response = await fetch(`https://swapi.dev/api/people/?search=${value}`);
+
+    const data = await response.json();
+
+    console.log(data.results);
+
+    setData(data.results);
+
+    
+
+  },2000),[])
+
+
+  const handleChange = (e) => {
+
+    const value = e.target.value;
+    setText(value);
+    fetchData(value);
+  }
+
 
   useEffect(() => {
-    const fetchPhotos = async () => {
-      const response = await fetch('https://jsonplaceholder.typicode.com/photos');
-      const data = await response.json();
-      console.log(data);
-      setPhotos(data);
-    }
 
-    fetchPhotos();
+    return () => {
+      fetchData?.cancel();
+    }
   },[])
+
   return (
     <>
-    {photos && photos?.length ? photos.map((photo) => <img loading='lazy' height={200} width={200} key={photo.id} src={photo.url}/>) : <span>Loding Images</span>}
+    <h1>Welcome to Star Wars</h1>
+    <input value={text} onChange={handleChange}/>
+    <br/>
+    <ul>
+    {data && data?.length ? data.map((item,index) => <li key={index}>{item.name}</li>) : <span>Loading</span>}
+    </ul>
     </>
   );
 };
