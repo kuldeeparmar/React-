501f91a2bc437c1ab7eba9dd41f933e6b223eda3
Kuldeep Parmar <kuldeepparmar246@gmail.com>
2024-08-19 20:36:34 +0530

    Explore Fetch Api and Optimization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import { Route,Routes,Link,useParams } from "react-router-dom"; 
 import Comp1 from './Comp1.jsx'
 import InputProps from "./InputProps.jsx";
@@ -6,11 +6,21 @@ import useOnline from './useOnline.js'
 
 const App = () => {
 
-  const isOnline = useOnline();
+ const [photos,setPhotos] = useState();
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+      const data = await response.json();
+      console.log(data);
+      setPhotos(data);
+    }
+
+    fetchPhotos();
+  },[])
   return (
     <>
-    {isOnline ? <div>I am Online</div> : <div>I am offline</div>}
-    {isOnline ? <div>I am in chat</div> : <div>I am not in chat</div>}
+    {photos && photos?.length ? photos.map((photo) => <img loading='lazy' height={200} width={200} key={photo.id} src={photo.url}/>) : <span>Loding Images</span>}
     </>
   );
 };
